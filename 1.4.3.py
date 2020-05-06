dce031665bd31e1697625141b8ca2ecfb92897e2
Bohdan <58623074+nc6d@users.noreply.github.com>
2020-05-06 16:00:58 +0300

    Update 1.4.3.py

--- 1.4.3.py
+++ 1.4.3.py
@@ -1,14 +1,30 @@
-"""3. Сформувати функцію для переведення натурального числа з десяткової системи
+"""1.4.3. Сформувати функцію для переведення натурального числа з десяткової системи
 числення у шістнадцятирічну."""
+import timeit
 
 
-def hex_converter(num):
+# recursion method
+def hex_converter_rec(num):
     num, modulo = num // 16, all_digs[num % 16]
     if num:
-        return hex_converter(num) + modulo
+        return hex_converter_rec(num) + modulo
     return modulo
+
+
+# iteration method
+def hex_converter_iter():
+    return hex(n)
 
 
 all_digs = "0123456789ABCDEF"  # all possible symbols of hex system
 n = int(input('Enter a digit to convert: '))
-print(n, 'in decimal =', hex_converter(n), 'in hex system')
+
+check = input('Enter 1 to use recursion and 2 to use iteration: ')
+if check == "1":
+    print(n, '=', hex_converter_rec(n))
+elif check == '2':
+    print(n, '=', ''.join(map(str, hex_converter_iter().replace('0x', '').upper())))
+
+# Execution time
+t = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(f"Program was executed by {t} seconds")
