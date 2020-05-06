9373b53cbdd6ee65f6693e5f38d76173669f2bc8
Bohdan <58623074+nc6d@users.noreply.github.com>
2020-05-06 15:59:21 +0300

    Update 1.4.1.py

--- 1.4.1.py
+++ 1.4.1.py
@@ -1,16 +1,27 @@
-"""1. Сформувати функцію для введення з клавіатури послідовності чисел і виведення
+"""1.4.1. Сформувати функцію для введення з клавіатури послідовності чисел і виведення
 її на екран у зворотному порядку (завершаючий символ послідовності – крапка)"""
+import timeit
 
 
+# recursion method
 # reverse entered string
-def reverse(s):
+def reverse_rec(s):
     if len(s) == 1:
         return s[0]
     else:
-        return s[len(s) - 1] + reverse(s[0:len(s) - 1])
+        return s[len(s) - 1] + reverse_rec(s[0:len(s) - 1])
 
 
-# enter elements till dot will be not detected
+# iteration method
+def reverse_iter(s):
+    line = ""
+    for i in s:
+        line = i + line
+    return line
+
+
+# enter elements
+# till dot will be not detected
 arr = []
 item = 0
 k = 0
@@ -19,6 +30,16 @@ while item != '.':
     item = input(f'Enter element #{k}: ')
     arr.append(item)
 
-# reformatting data to present
-print(''.join(map(str, arr)))
-print(reverse(arr))
+print('Default order:\n', ''.join(map(str, arr)))
+
+check = (input("Enter 1 to use recursion and 2 to use iteration: "))
+if check == "1":
+    print('-' * 30, '\n', 'Default order:\n', ''.join(map(str, arr)))
+    print('\n', 'Reversed order:\n', reverse_rec(arr))
+elif check == '2':
+    print('-' * 30, '\n', 'Default order:\n', ''.join(map(str, arr)))
+    print('\n', 'Reversed order:\n', reverse_iter(arr))
+
+# Execution time
+t = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(f"Program was executed by {t} seconds")
