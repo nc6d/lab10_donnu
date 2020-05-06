b71104d8558ccf254ef9d82b0c1880e7b715a644
Bohdan <58623074+nc6d@users.noreply.github.com>
2020-05-06 15:57:21 +0300

    Update 1.3.1.py

--- 1.3.1.py
+++ 1.3.1.py
@@ -1,11 +1,33 @@
-"""1. Сформувати функцію, що буде обчислювати факторіал заданого користувачем
+"""1.3.1. Сформувати функцію, що буде обчислювати факторіал заданого користувачем
 натурального числа n."""
+import timeit
 
-def  factorial(n):
+
+# recursion method
+def factorial_rec(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial_rec(n - 1)
 
 
-print('The factorial is', factorial(int(input('Enter an digit: '))))
+# iteration method
+def factorial_iter():
+    n = int(input('Enter an digit: '))
+    factorial = 1
+    while n > 1:
+        factorial *= n
+        n -= 1
+    print('The factorial is', factorial)
+
+
+check = input('Enter 1 to use recursion and 2 to use iteration: ')
+
+if check == '1':
+    print('The factorial is', factorial_rec(int(input('Enter an digit: '))))
+elif check == '2':
+    factorial_iter()
+
+# Execution time
+t = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(f"Program was executed by {t} seconds")
